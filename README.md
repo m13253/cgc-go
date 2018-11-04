0d48ec028ba099aed7bd3cf696a231e1e672d490
Star Brilliant <coder@poorlab.com>
2018-11-04 14:24:34 +0800

    Add documentation

---
+++ README.md
@@ -0,0 +1,12 @@
+[![](https://godoc.org/github.com/m13253/cgc-go?status.svg)](http://godoc.org/github.com/m13253/cgc-go)
+
+Package cgc is a package that allows you pass a function for another goroutine
+to execute it.
+
+It is useful when some logic must be run on a separate thread and you want to
+wait for the result, or you want to cancel a foreign function with Go's context
+cancellation mechanism.
+
+The package provides a event queue named `Executor`. You can make use of
+`Executor` to listen to requests from one goroutine, and send requests to it
+from any other goroutines.
--- cgc.go
+++ cgc.go
@@ -21,6 +21,17 @@
    DEALINGS IN THE SOFTWARE.
 */
 
+// Package cgc is a package that allows you pass a function for another
+// goroutine to execute it.
+//
+// It is useful when some logic must be run on a separate thread and you want to
+// wait for the result, or you want to cancel a foreign function with Go's
+// context cancellation mechanism.
+//
+// The package provides a event queue named Executor. You can make use of
+// Executor to listen to requests from one goroutine, and send requests to it
+// from any other goroutines. Please refer to documentations of each function
+// for details.
 package cgc
 
 import (
@@ -31,7 +42,7 @@ import (
 )
 
 type (
-	// Func is a function to submit to another goroutine to execute
+	// Func is a function to submit to another goroutine to execute.
 	Func func(ctx context.Context) (interface{}, error)
 
 	result struct {
@@ -39,30 +50,45 @@ type (
 		err error
 	}
 
-	// Request is a message body passed from the caller to the callee
+	// Request is a message body passed from the caller to the callee.
 	Request struct {
 		Func    Func
 		Context context.Context
 		result  chan<- *result
 	}
 
-	// Executor is a cross-goroutine execution unit
+	// Executor is a cross-goroutine execution unit.
 	Executor chan *Request
 )
 
-// New creates a new unbuffered Executor
+// New creates a new unbuffered Executor.
 func New() Executor {
 	return make(Executor)
 }
 
-// NewBuffered creates a new Executor with specified buffer length
+// NewBuffered creates a new Executor with specified buffer length.
 func NewBuffered(bufferLength uint) Executor {
 	return make(Executor, bufferLength)
 }
 
-// RunLoop keeps executing requests from the executor until ctx is canceled
+// RunLoop keeps executing requests from the executor until ctx is canceled.
 //
-// This function should be called from the callee goroutine
+// This function should be called from the callee goroutine, either ctx or
+// r.Context may cancel the inner function.
+//
+// Instead of using RunLoop, you can also use
+//     loop:
+//         for {
+//             select {
+//                 case req := <-executor:
+//                     cgc.RunOneRequest(ctx, req)
+//                 case <-ctx.Done():
+//                     break loop
+//                 /* case other events: */
+//             }
+//         }
+// manually if the callee goroutine has more things to do than just listening to
+// requests.
 func (e Executor) RunLoop(ctx context.Context) error {
 	for {
 		err := e.RunOnce(ctx)
@@ -75,9 +101,14 @@ func (e Executor) RunLoop(ctx context.Context) error {
 	}
 }
 
-// RunOnce executes one request from the executor
+// RunOnce executes one request from the executor.
 //
-// This function should be called from the callee goroutine
+// This function should be called from the callee goroutine, either ctx or
+// r.Context may cancel the inner function.
+//
+// Instead of using RunOnce, you can also use
+//     req := <-executor
+//     cgc.RunOneRequest(ctx, req)
 func (e Executor) RunOnce(ctx context.Context) error {
 	select {
 	case r, ok := <-e:
@@ -90,9 +121,10 @@ func (e Executor) RunOnce(ctx context.Context) error {
 	}
 }
 
-// Submit submits a request to the executor and wait for the result
+// Submit submits a request to the executor and wait for the result.
 //
-// This function should be called from the caller goroutine
+// This function should be called from the caller goroutine, either ctx or the
+// context at the callee goroutine may cancel the request.
 func (e Executor) Submit(ctx context.Context, f Func) (interface{}, error) {
 	resultChan := make(chan *result, 1)
 	select {
@@ -115,9 +147,11 @@ func (e Executor) Submit(ctx context.Context, f Func) (interface{}, error) {
 	}
 }
 
-// SubmitNoWait submits a request, wait for the request to be received, but does not wait for the result
+// SubmitNoWait submits a request, wait for the request to be received, but does
+// not wait for the result.
 //
-// This function should be called from the caller goroutine
+// This function should be called from the caller goroutine, either ctx or the
+// context at the callee goroutine may cancel the request.
 func (e Executor) SubmitNoWait(ctx context.Context, f Func) error {
 	select {
 	case e <- &Request{
@@ -131,9 +165,13 @@ func (e Executor) SubmitNoWait(ctx context.Context, f Func) error {
 	return nil
 }
 
-// RunOneRequest executes on request that is already extracted from an executor
+// RunOneRequest executes on request that is already extracted from an executor.
 //
-// Either ctx or r.Context may cancel the inner function
+// To extract a request from an executor, use
+//     req := <-executor
+//
+// This function should be called from the callee goroutine, either ctx or
+// r.Context may cancel the inner function.
 func RunOneRequest(ctx context.Context, r *Request) error {
 	joinedCtx, joinedCancel := ctx, context.CancelFunc(nil)
 	if ctx != r.Context {
